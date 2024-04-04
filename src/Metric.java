fa373be30dc11869cad54061f0f617c6b8129682
Ari <michael.al.franklin@gmail.com>
2024-04-04 16:33:20 -0400

    reformated code

--- src/Metric.java
+++ src/Metric.java
@@ -3,29 +3,27 @@ import java.util.ArrayList;
 public class Metric {
 
 
+    //this method verifies the distribution that the values follow the formula for a Gaussian distribution.
+// 68% of values should fall within 1 standard deviation, 95% percent within 2, and 99% within three.
+    public static String verifyDistribution(ArrayList<Double> arr, double mean, double SD,
+                                            double numberOfSD) {
 
-public static String verifyDistribution(ArrayList<Double> arr, double mean, double SD,
-                                        double numberOfSD){
+        double counter = 0;
+        for (Double var : arr) {
 
-double counter =0;
-    for(Double var: arr){
+            if (var <= (mean + SD * numberOfSD) && var >= (mean - SD * numberOfSD)) {
+                counter++;
+            }
 
-        if(var <= (mean+SD*numberOfSD) && var >= (mean-SD*numberOfSD)){
-            counter++;
         }
 
+        double percentage = counter / arr.size() * 100;
+
+
+        return String.format("%.3f", percentage);
+
+
     }
-
-    double percentage = counter / arr.size() * 100;
-
-
-
-    return String.format("%.3f", percentage);
-
-
-}
-
-
 
 
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -3,93 +3,96 @@ import java.util.Random;
 
 public class Simulation {
 
-private int [] bins;
+    private int[] bins;
 
 
- //This method populates the array with randomly distributed variables with amount specified by the int param.
- public void generateNormalRandomNumbers(ArrayList<Double> arr, int numberOfRandomVariables){
+    //This method populates the array with randomly distributed variables with amount specified by the int param.
+    public void generateNormalRandomNumbers(ArrayList<Double> arr, int numberOfRandomVariables) {
 
-     Random rand = new Random();
-    for(int i =0; i<numberOfRandomVariables; i++){
+        Random rand = new Random();
+        for (int i = 0; i < numberOfRandomVariables; i++) {
 
-        double normalRandom = rand.nextGaussian();
+            double normalRandom = rand.nextGaussian();
 
-        arr.add(normalRandom);
+            arr.add(normalRandom);
+
+        }
 
     }
-
- }
 
 
     //This method takes a randomly distributed populated array and number of bins
     // and calculates where the number should be distributed.
     // Returns an array with the correct distribution of bins
-    public int [] makeBins(ArrayList<Double> arr, int numberOfBins){
+    public int[] makeBins(ArrayList<Double> arr, int numberOfBins) {
 
-     double binRange = (getRange(arr)/numberOfBins);
-
-
-     double smallest = getMin(arr);
+        double binRange = (getRange(arr) / numberOfBins);
 
 
-     this.bins = new int [numberOfBins];
+        double smallest = getMin(arr);
 
-     for(Double randValue : arr){
 
-         int index = getBin(binRange, smallest, randValue);
+        this.bins = new int[numberOfBins];
 
-         //adjusting for upper bound case
-         if(index == numberOfBins) { index--;}
+        for (Double randValue : arr) {
 
-             this.bins[index]++;
+            int index = getBin(binRange, smallest, randValue);
 
-     }
+            //adjusting for upper bound case
+            if (index == numberOfBins) {
+                index--;
+            }
 
-    return this.bins;
+            this.bins[index]++;
+
+        }
+
+        return this.bins;
 
     }//end make bin
 
     //getBin() performs the calculation to find the correct distance from the smallest
     //to place the randomValue in the correct index
-    public int getBin(double binSize, double min,  double randomValue){
-    return  (int) Math.floor((randomValue - min) / binSize) ;
+    public int getBin(double binSize, double min, double randomValue) {
+        return (int) Math.floor((randomValue - min) / binSize);
 
- }
+    }
 
     //this method prints the bin array and therefore the distribution of the bins
-    public void printBin(){
-        if (this.bins[0]!= 0) {
+    public void printBin() {
+        if (this.bins[0] != 0) {
             for (Integer var : this.bins) {
 
                 System.out.println(var);
             }
-        } else{
+        } else {
             System.out.println("Bins are empty");
         }
 
     }
+
     //calculates the smallest value in the array
     public double getMin(ArrayList<Double> arr) {
-     double smallest = 0;
-     if (!arr.isEmpty()){
-         smallest = arr.getFirst();
-         for (Double randomValue : arr) {
+        double smallest = 0;
+        if (!arr.isEmpty()) {
+            smallest = arr.getFirst();
+            for (Double randomValue : arr) {
 
-             if (randomValue < smallest) {
-                 smallest = randomValue;
-             }
-         }
-    }
+                if (randomValue < smallest) {
+                    smallest = randomValue;
+                }
+            }
+        }
         return smallest;
-     }
+    }
 
-     //calculates the largest element in the array.
-    public double getMax(ArrayList<Double> arr){
+    //calculates the largest element in the array.
+    public double getMax(ArrayList<Double> arr) {
 
 
-     double largest= 0;
+        double largest = 0;
 
-        if (!arr.isEmpty()){
+        if (!arr.isEmpty()) {
 
             largest = arr.getFirst();
             for (Double randomValue : arr) {
@@ -101,10 +104,11 @@ private int [] bins;
         }
         return largest;
     }
-    //calculates and returns a double for the range of values within the array
-    public double getRange(ArrayList<Double> arr){
 
-     return (getMax(arr)-getMin(arr));
+    //calculates and returns a double for the range of values within the array
+    public double getRange(ArrayList<Double> arr) {
+
+        return (getMax(arr) - getMin(arr));
 
     }
 
